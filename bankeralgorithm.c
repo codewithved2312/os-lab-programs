1c79c44f74e98447f742d4213b5bdd0c3771198f
Vyas Ved Himanshu <vedcodesgithubid22312@gmail.com>
2025-10-07 13:09:21 +0530

    Update bankeralgorithm.c

--- bankeralgorithm.c
+++ bankeralgorithm.c
@@ -1,96 +1,89 @@
-// Banker's algorithm
-// Deadlock avoidance and detection
-
-#include<stdio.h>
+// Banker's Algorithm - Deadlock Avoidance & Detection
+#include <stdio.h>
 
 int main() {
-    int n,m; // no. of processes, no. of resources
-    int i,j,k; // to iterate over loop
-    printf("enter number of processes: ");
-    scanf("%d",&n);
-    printf("enter number of resources: ");
-    scanf("%d",&m);
-    
-    int alloc[n][m];  // allocation matrix
-    int max[n][m];  // max matrix
-    int avail[m];  // available resources
-    int need[n][m];  // need matrix
-    
-    // read allocation matrix
-    printf("\nenter allocation matrix\n");
-    for(i=0; i<n; i++) {
-        printf("process p%d",i);
-        for(j=0; j<m; j++){
-            scanf("%d",&alloc[i][j]);
+    int n, m;
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+    printf("Enter number of resources: ");
+    scanf("%d", &m);
+
+    int alloc[n][m], max[n][m], avail[m], need[n][m];
+    int f[n], ans[n], ind = 0;
+
+    // Input Allocation Matrix
+    printf("\nEnter Allocation Matrix:\n");
+    for (int i = 0; i < n; i++) {
+        printf("Process P%d: ", i);
+        for (int j = 0; j < m; j++) {
+            scanf("%d", &alloc[i][j]);
         }
     }
-    
-    // read max matrix
-    printf("\nenter max matrix\n");
-    for(i=0; i<n; i++) {
-        printf("process p%d",i);
-        for(j=0; j<m; j++){
-            scanf("%d",&max[i][j]);
+
+    // Input Max Matrix
+    printf("\nEnter Max Matrix:\n");
+    for (int i = 0; i < n; i++) {
+        printf("Process P%d: ", i);
+        for (int j = 0; j < m; j++) {
+            scanf("%d", &max[i][j]);
         }
     }
-    
-    // read available matrix
-    printf("\nenter available matrix\n");
-    for(i=0; i<n; i++) {
-        printf("process p%d",i);
-        scanf("%d",&avail[i]);
+
+    // Input Available Resources
+    printf("\nEnter Available Resources:\n");
+    for (int j = 0; j < m; j++) {
+        scanf("%d", &avail[j]);
     }
-    
-    // calc need matrix
-    for(i=0; i<n; i++) {
-        for(j=0; j<m; j++){
+
+    // Calculate Need Matrix
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
             need[i][j] = max[i][j] - alloc[i][j];
         }
     }
-    
-    //banker algorithm
-    int f[n],ans[n], ind=0;
-    for(k=0; k<n; k++) {
-        f[k]=0;
+
+    // Initialize Finish array
+    for (int k = 0; k < n; k++)
+        f[k] = 0;
+
+    // Banker's Algorithm
+    for (int k = 0; k < n; k++) {
+        for (int i = 0; i < n; i++) {
+            if (f[i] == 0) {
+                int flag = 0;
+                for (int j = 0; j < m; j++) {
+                    if (need[i][j] > avail[j]) {
+                        flag = 1;
+                        break;
+                    }
+                }
+                if (flag == 0) {
+                    ans[ind++] = i;
+                    for (int y = 0; y < m; y++)
+                        avail[y] += alloc[i][y];
+                    f[i] = 1;
+                }
+            }
+        }
     }
-    
-    for(k=0; k<n; k++) {
-       for(i=0; i<n; i++) {
-           if(f[i]==0) {
-               int flag = 0;
-               for(j=0; j<m; j++) {
-                   if(need[i][j] > avail[j]){
-                       flag=1;
-                       break;
-                   }
-               }
-               if(flag == 0) {
-                   ans[ind++] = i;
-                   for(j=0;j<m;j++) {
-                       avail[j] += alloc[i][j];
-                   }
-                   f[i]=1;
-               }
-           }
-       }
+
+    // Check if system is in a safe state
+    int safe = 1;
+    for (int i = 0; i < n; i++) {
+        if (f[i] == 0) {
+            safe = 0;
+            printf("\nSystem is NOT in a safe state!\n");
+            break;
+        }
     }
-    
-// check if system is in safe state
-  int flag = 1;
-  for(i=0; i<n; i++) {
-      if(f[i] == 0) {
-          flag = 0;
-          printf("\n system is not in safe state \n");
-          break;
-      }
-  }
-  
-  if(flag == 1) {
-      printf("\n system is in safe state \n");
-      for(i=0; i<n; i++) {
-          printf("p%d ->",ans[i]);
-      }
-      printf("p%d\n",ans[n-1]);
-  }
-   return 0; 
+
+    if (safe == 1) {
+        printf("\nSystem is in a SAFE STATE.\nSafe sequence is: ");
+        for (int i = 0; i < n - 1; i++) {
+            printf("P%d -> ", ans[i]);
+        }
+        printf("P%d\n", ans[n - 1]);
+    }
+
+    return 0;
 }
